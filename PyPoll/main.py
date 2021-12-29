1f56d81f2727fcddb404ec72d65e3108ac64f62c
Cynniminni <radicalcyn@gmail.com>
2021-12-28 16:16:46 -0800

    Add PyPoll answer

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,54 +1,98 @@
 import os
-import pandas
+import csv
 
-def read_election_csv_file() -> pandas.DataFrame:
-    """
-    Reads the csv file and loads it into a variable of type DataFrame.
-    If the file is not found, raise a FileNotFoundError and end execution.
-    :return:
-    """
+def read_csv_file():
     current_working_directory = os.getcwd()
     file_path = os.path.join(current_working_directory, "Resources", "election_data.csv")
     if os.path.exists(file_path):
-        csv_data = pandas.read_csv(file_path)
+        # Read csv file and save it as a list of lists
+        csv_data = []
+        with open(file_path) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+            for row in csv_reader:
+                csv_data.append(row)
+
+        # Drop the header row
+        csv_data.pop(0)
+
+        # Return the csv data as a list
         return csv_data
     else:
         raise FileNotFoundError(f"File not found at: {file_path}")
 
 def total_votes(csv_data):
-    pass
+    """
+    Returns the total number of votes, which is equivalent to the number of rows in the csv_data.
+    :param csv_data:
+    :return:
+    """
+    return len(csv_data)
 
-def khan_total_votes(csv_data):
-    pass
+def count_total_candidate_votes(csv_data: list, candidate_name: str):
+    """
+    Returns the total number of votes for a given candidate name.
+    :return:
+    """
+    count = 0
+    for row in csv_data:
+        candidate = row[2]
+        if candidate == candidate_name:
+            count += 1
+    return count
 
-def correy_total_votes(csv_data):
-    pass
+def calculate_vote_percentage(total: int, votes: int):
+    result = (votes / total) * 100
+    return f"{result:.3f}"
 
-def li_total_votes(csv_data):
-    pass
-
-def o_tooley_total_votes(csv_data):
-    pass
-
-def winner(khan, correy, li, o_tooley):
-    pass
+def determine_winner(khan, correy, li, o_tooley):
+    # Determine winner
+    biggest_value = max(khan, correy, li, o_tooley)
+    if biggest_value == khan:
+        return "Khan"
+    elif biggest_value == correy:
+        return "Correy"
+    elif biggest_value == li:
+        return "Li"
+    elif biggest_value == o_tooley:
+        return "O'Tooley"
+    else:
+        print("ERROR, please check values are all correct")
 
 def print_election_results():
     """
     This is the function that will print the end result for the homework assignment.
     :return:
     """
-    csv_data = read_election_csv_file()
+    # Read csv file and return it as a list
+    csv_data = read_csv_file()
+
+    # Get counts for each candidate
+    total_votes_count = total_votes(csv_data)
+    khan_total_votes = count_total_candidate_votes(csv_data, "Khan")
+    correy_total_votes = count_total_candidate_votes(csv_data, "Correy")
+    li_total_votes = count_total_candidate_votes(csv_data, "Li")
+    o_tooley_total_votes = count_total_candidate_votes(csv_data, "O'Tooley")
+
+    # Get percentages for each candidate
+    khan_percentage = calculate_vote_percentage(total_votes_count, khan_total_votes)
+    correy_percentage = calculate_vote_percentage(total_votes_count, correy_total_votes)
+    li_percentage = calculate_vote_percentage(total_votes_count, li_total_votes)
+    o_tooley_percentage = calculate_vote_percentage(total_votes_count, o_tooley_total_votes)
+
+    # Get winner
+    winner = determine_winner(khan_percentage, correy_percentage, li_percentage, o_tooley_percentage)
+
+    # Print election results
     print("Election Results")
     print("-------------------------")
-    print(f"Total Votes: ")
+    print(f"Total Votes: {total_votes_count}")
     print("-------------------------")
-    print(f"Khan: ")
-    print(f"Correy: ")
-    print(f"Li: ")
-    print(f"O'Tooley: ")
+    print(f"Khan: {khan_percentage}% ({khan_total_votes})")
+    print(f"Correy: {correy_percentage}% ({correy_total_votes})")
+    print(f"Li: {li_percentage}% ({li_total_votes})")
+    print(f"O'Tooley: {o_tooley_percentage}% ({o_tooley_total_votes})")
     print("-------------------------")
-    print(f"Winner: ")
+    print(f"Winner: {winner}")
     print("-------------------------")
 
 
